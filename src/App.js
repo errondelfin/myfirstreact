e24f4b2dbbb2adf1fe7f1036492b59d4741df06e
errondelfin <90945641+errondelfin@users.noreply.github.com>
2021-11-10 13:48:48 +0800

    button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Greet from './components/Greet';
 function App() {
   return (
     <div className="App">
@@ -8,6 +8,7 @@ function App() {
         <img src={'https://i.postimg.cc/C5LbXXsb/final-614fdad5f79ba90085f11986-76434-removebg-preview.png'} className="App-logo" alt="logo" />
         <p>
           Pogi ako. - erron
+          <Greet></Greet>
         </p>
         <a
           className="App-link"
---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+class Button extends React.component{
+    constructor(){
+        super()
+        this.state = {
+            count : 0
+        }
+    }
+        addCount = () =>{
+        this.setState(
+                {
+                    count : this.state.count + 1
+                }
+                )
+            
+        
+    this.state.count = this.state.count + 1
+    }
+  
+    render(){
+        return(
+               <div>
+            <button onClick ={this.addCount}>click me :) </button>
+            <p>counting = {this.state.count}</p>
+            </div>
+        )
+    }
+}
+export default Button
